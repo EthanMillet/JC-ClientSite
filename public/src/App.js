289b5efb0f91c72f65ef6ec9e13f2cd7156b39b3
Ethan <ethanamillet@gmail.com>
2024-01-31 13:55:28 -0700

    help me help me help meeeee

--- public/src/App.js
+++ public/src/App.js
@@ -1,6 +1,6 @@
+import './App.css';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import {
   ApolloClient,
   InMemoryCache,
@@ -15,27 +15,39 @@ import Contact from './components/pages/contact'
 import About from './components/pages/about'
 import Nav from './components/nav'
 import Footer from './components/footer';
-import reportWebVitals from './reportWebVitals';
 
+const httpLink = createHttpLink({
+  uri: process.env.herokuLink || 'http://localhost:3001/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
     return (
- <>
+ <ApolloProvider client={client}>
  <Nav/>
   <Routes>
     <Route path='/' element={<Main/>}></Route>
     <Route path='/contact' element={<Contact/>}></Route>
     <Route path='/about' element={<About/>}></Route>
-    <Route path="*" element={<Navigate to="/" />}></Route>
+
   </Routes>
   <Footer/>
- </>
+ </ApolloProvider>
     )
     };
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
 
 export default App;
