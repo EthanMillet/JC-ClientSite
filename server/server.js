470f402c276707298c601d773a2d20ce68fa83d3
Ethan <ethanamillet@gmail.com>
2024-01-31 12:00:36 -0700

    adding heroku bug fix

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const nodemailer = require("nodemailer");
 const app = express();
 const cors = require("cors");
 
+const PORT = process.env.PORT || 3001;
+
 app.use(express.json());
 app.use(cors());
 
@@ -47,6 +49,6 @@ let transporter = nodemailer.createTransport({
    });
    
    const port = 3001;
-   app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+   app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
    });
